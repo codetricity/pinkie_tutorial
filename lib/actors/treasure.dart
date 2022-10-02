bbeb8a6edb2e08e27e60481457566c417e10e854
Craig <craig@oppkey.com>
2022-10-02 07:36:45 -0700

    11 animation

--- lib/actors/treasure.dart
+++ lib/actors/treasure.dart
@@ -5,7 +5,7 @@ import 'package:pinkie_tutorial/actors/pinkie.dart';
 import 'package:pinkie_tutorial/blocs/score/score_bloc.dart';
 import 'package:pinkie_tutorial/flame_layer/pinkie_game.dart';
 
-class Treasure extends SpriteComponent
+class Treasure extends SpriteAnimationComponent
     with
         HasGameRef<PinkieGame>,
         CollisionCallbacks,
@@ -16,43 +16,88 @@ class Treasure extends SpriteComponent
     debugMode = true;
   }
 
-  late Sprite redGem;
-  late Sprite pinkGem;
-  late Sprite greenGem;
+  // late Sprite redGem;
+  // late Sprite pinkGem;
+  // late Sprite greenGem;
+
+  late SpriteAnimation redGemAnimation;
+  late SpriteAnimation pinkGemAnimation;
+  late SpriteAnimation greenGemAnimation;
+
+  Future<List<Sprite>> loadGemSprites(GemType gemType) async {
+    List<Sprite> sprites = [];
+    String spriteFileName = '';
+    switch (gemType) {
+      case GemType.red:
+        spriteFileName = 'gems/red';
+        break;
+      case GemType.pink:
+        spriteFileName = 'gems/pink';
+        break;
+      case GemType.green:
+        spriteFileName = 'gems/green';
+        break;
+    }
+
+    for (int i = 1; i < 61; i++) {
+      if (i < 10) {
+        sprites.add(await Sprite.load('$spriteFileName/000$i.png'));
+      } else {
+        sprites.add(await Sprite.load('$spriteFileName/00$i.png'));
+      }
+    }
+    return sprites;
+  }
 
   @override
   Future<void> onLoad() async {
     await super.onLoad();
     add(RectangleHitbox());
-    redGem = await gameRef.loadSprite('red_gem.png');
-    pinkGem = await gameRef.loadSprite('pink_gem.png');
-    greenGem = await gameRef.loadSprite('green_gem.png');
 
-    sprite = redGem;
+    redGemAnimation = SpriteAnimation.spriteList(
+        await loadGemSprites(GemType.red),
+        stepTime: 0.1);
+    pinkGemAnimation = SpriteAnimation.spriteList(
+        await loadGemSprites(GemType.pink),
+        stepTime: 0.1);
+    greenGemAnimation = SpriteAnimation.spriteList(
+        await loadGemSprites(GemType.green),
+        stepTime: 0.1);
+
+    // redGem = await gameRef.loadSprite('red_gem.png');
+    // pinkGem = await gameRef.loadSprite('pink_gem.png');
+    // greenGem = await gameRef.loadSprite('green_gem.png');
+
+    // sprite = redGem;
+    animation = redGemAnimation;
   }
 
   @override
   void onNewState(ScoreState state) {
     switch (state.gemType) {
       case GemType.pink:
-        sprite = pinkGem;
+        animation = pinkGemAnimation;
+        // sprite = pinkGem;
         break;
       case GemType.green:
-        sprite = greenGem;
+        // sprite = greenGem;
+        animation = greenGemAnimation;
         break;
       case GemType.red:
-        sprite = redGem;
+        // sprite = redGem;
+        animation = redGemAnimation;
         break;
     }
 
     super.onNewState(state);
   }
 
-  @override
-  void update(double dt) {
-    // x += 1;
-    // y += 1;
-  }
+  // @override
+  // void update(double dt) {
+  //   // x += 1;
+  //   // y += 1;
+  //   super.update(dt);
+  // }
 
   @override
   void onCollisionStart(
